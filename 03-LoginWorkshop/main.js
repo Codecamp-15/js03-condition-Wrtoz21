4bb6a381b10e671bdfd542cae0167a8b1371011a
warittorn <92501411+Wrtoz21@users.noreply.github.com>
2023-08-09 03:38:10 +0700

    Update main.js

---
+++ 03-LoginWorkshop/main.js
@@ -0,0 +1,17 @@
+let user = prompt("Input your user");
+console.log(user);
+if (user === null || user.trim() === '' )
+{
+    alert("guest")
+}
+else if (user =='codecamp')
+{
+   let password = +prompt("Input your password")
+   password != 123456;
+   alert('Wrong')
+}
+
+else if(user !='codecamp'){ user == 'guest'}
+
+alert('welcome '+user)
+
