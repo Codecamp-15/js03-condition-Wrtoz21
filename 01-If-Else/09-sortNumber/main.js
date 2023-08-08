2a4c34492e89361ea5e6cd45eabd21a1dc495fe1
Wrtoz21 <kobclimax@gmail.com>
2023-08-08 08:33:28 +0700

    JS3 homework on processing

---
+++ 01-If-Else/09-sortNumber/main.js
@@ -0,0 +1,9 @@
+
+let input1 = (+(prompt("input1")));
+let input2 = (+(prompt("input2")));
+let input3 = (+(prompt("input3")));
+
+let A = [input1,input2,input3];
+A.sort((a, b) => a - b);
+console.log(A);
+// processing
